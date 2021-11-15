aa40b4aa48f2036356a0172d57eafb00be2670d2
Marat Safin <safin.marat62@yandex.ru>
2021-11-15 03:59:54 +0300

    Последнее изменение

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,27 @@
 const profileButton = document.querySelector(".profile__button");
 const popup = document.querySelector(".popup");
 const popupCloseButton = popup.querySelector(".popup__close");
-const popupOverlay = popup.querySelector(".popup__overlay");
-const title = document.querySelector(".lead__title");
-
-
+const popupButton = document.querySelector(".popup__button");
+const nameInput = document.querySelector(".popup__input-name");
+const jobInput = document.querySelector(".popup__input-job");
+const title = document.querySelector(".profile__title");
+const subTitle = document.querySelector(".profile__subtitle");
 
 function open() {
-    popup.classList.add("popup_opened");
+	popup.classList.add("popup__open");
 }
 
 function close() {
-    title.textContent = "NEW CONTENT";
-    popup.classList.remove("popup_opened");
+	popup.classList.remove("popup__open");
 }
 
-if (profileButton) {
-    profileButton.addEventListener('click', open);
-}
+profileButton.addEventListener("click", open);
+popupCloseButton.addEventListener("click", close);
 
-popupCloseButton.addEventListener('click', close);
-popupOverlay.addEventListener('click', close);
+function save(evt) {
+	evt.preventDefault();
+	title.textContent = nameInput.value;
+	subTitle.textContent = jobInput.value;
+}
+popupButton.addEventListener('click', save);
+popupButton.addEventListener('click', close);
