0a63cfa973454ed32c00018927bc8d4d6bdaf74c
Marat Safin <safin.marat62@yandex.ru>
2022-03-28 21:49:27 +0300

    изм1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,123 +1,122 @@
 
 import './index.css';
 import { Card } from '../components/Card.js';
-import {FormValidator} from "../components/FormValidator.js";
+import { FormValidator } from "../components/FormValidator.js";
 import { PopupWithForm } from '../components/PopupWithForm.js';
 import { Section } from '../components/Section.js';
 import { PopupWithImage } from '../components/PopupWithImage.js';
 import { UserInfo } from '../components/UserInfo.js';
 import {
 	profileButton,
-    popupProfile,
-    formplace,
-    nameInput,
-    jobInput, 
-    title, 
-    subTitle, 
+	popupProfile,
+	formplace,
+	nameInput,
+	jobInput,
+	title,
+	subTitle,
 	listContainer,
 	popupadd,
 	enableValidation,
 	imageInput,
-    placeInput,
+	placeInput,
 	initialCards
-	
-
-  } from '../utils/constants.js';
 
 
+} from '../utils/constants.js';
+const renderCard = (item) => {
+	const newCard = new Card({
+		data: item,
+		popupSelector: '.template',
+		handleCardClick: () => popupWithImage.open(item)
+	});
+	const initialCard = newCard.generate();
+	return initialCard;
+};
 
-  const section = new Section({
+
+const section = new Section({
 	items: initialCards,
 	renderer: renderCard,
-  },
+},
 	listContainer
-  );
-  
-  section.renderItems();
+);
+
+section.renderItems();
 
 
 // Обработчик сабмита формы добавления места
-const handlePlaceFormSubmit = (data) =>   
-{section.addItem(renderCard({
-    name: data.place,
-    link: data.link
-  }))
-	
+const handlePlaceFormSubmit = (data) => {
+	section.addItem(renderCard({
+		name: data.place,
+		link: data.link
+	}))
 
-  
+
+
 	// addCard.renderItems();
 	addCardFormValidator.deactivateButton();
 	popupWithPlaceForm.close();
 	popupWithPlaceForm.removeEventListener();
-  };
+};
 
 
 // Обработчик формы редактирования профиля
 const userInfo = new UserInfo({
 	profileTitle: title,
 	profilesubTitle: subTitle
-  });
-  
-  const handleProfileFormSubmit = () => {
-	const name = nameInput.value;
-	const job = jobInput.value;
+});
+
+const handleProfileFormSubmit = (data) => {
+	const { name, job } = data;
 	userInfo.setUserInfo(name, job);
 	popupWithProfileForm.close();
-  };
-  
-  // Попапы
-  const popupWithImage = new PopupWithImage('.popup_type_img');
-  const popupWithProfileForm = new PopupWithForm('.popup_type_profile', 
+};
+
+// Попапы
+const popupWithImage = new PopupWithImage('.popup_type_img');
+const popupWithProfileForm = new PopupWithForm('.popup_type_profile',
 	handleProfileFormSubmit
-  );
-  const popupWithPlaceForm = new PopupWithForm('.popup_type_place',
+);
+const popupWithPlaceForm = new PopupWithForm('.popup_type_place',
 	handlePlaceFormSubmit
-  );
-  
-  // Добавление карточки
-  const renderCard = (item) => {
-	const newCard = new Card({
-	  data: item,
-	  popupSelector: '.template',
-	  handleCardClick: () => popupWithImage.open(item)
-	});
-	const initialCard = newCard.generate();
-	return initialCard;
-  };
-  
-  const cardList = new Section({
+);
+
+// Добавление карточки
+
+
+const cardList = new Section({
 	items: initialCards.reverse(),
 	renderer: renderCard,
-  },
+},
 	listContainer
-  );
-  
-  cardList.renderItems();
-  
-  // Валидация форм
-  const editProfileFormValidator = new FormValidator(enableValidation, popupProfile);
-  const addCardFormValidator = new FormValidator(enableValidation, formplace);
-  
-  editProfileFormValidator.enableValidation();
-  addCardFormValidator.enableValidation();
-  
-  // Слушатели формы редактирования профиля
-  profileButton.addEventListener('click', () => {
-	const { name, job } = userInfo.getUserInfo() 
+);
+
+cardList.renderItems();
+
+// Валидация форм
+const editProfileFormValidator = new FormValidator(enableValidation, popupProfile);
+const addCardFormValidator = new FormValidator(enableValidation, formplace);
+
+editProfileFormValidator.enableValidation();
+addCardFormValidator.enableValidation();
+
+// Слушатели формы редактирования профиля
+profileButton.addEventListener('click', () => {
+	const { name, job } = userInfo.getUserInfo()
 	nameInput.value = name
 	jobInput.value = job
 	editProfileFormValidator.activateButton();
 	editProfileFormValidator.resetErrors();
 	popupWithProfileForm.open();
-  });
-    
-  // Слушатели формы добавления карточки
-  popupadd.addEventListener('click', () => {
+});
+
+// Слушатели формы добавления карточки
+popupadd.addEventListener('click', () => {
 	addCardFormValidator.resetErrors();
 	popupWithPlaceForm.open();
 	addCardFormValidator.deactivateButton();
-  });
-  
-  popupWithPlaceForm.setEventListener()
-  popupWithProfileForm.setEventListener()
-  popupWithImage.setEventListener()
+});
+
+popupWithPlaceForm.setEventListener()
+popupWithProfileForm.setEventListener()
+popupWithImage.setEventListener()
